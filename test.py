b4b97e59533ace80ae7b81dcb20f3ec994ff8d3f
VladNechetov <94560492+VladNechetov@users.noreply.github.com>
2021-11-20 12:20:46 +0200

    test.py
    Changes

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+import time
+from pynput
+
+def move ():
+  pass 
+  
+def draw ():
+  print ('*', shift + 'D')
+
+<<<<<< HEAD
+something
+
+
+while True: 
+  move ()
+  draw ()
+  time.sleep (0.1)  
