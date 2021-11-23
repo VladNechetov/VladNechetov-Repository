18d92fe0f10e07bc2e69f3a9e042af6920c164c4
VladNechetov <94560492+VladNechetov@users.noreply.github.com>
2021-11-23 18:31:00 +0200

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,40 @@
+import os
+import time
+from pynput
+
+bullets = [
+  {'x': 5, 'y': 6}, 
+  {'x': 10, 'y':3}
+]
+
+
+bricks = [
+  {'x': 5, 'val': 9},
+  {'x': 8, 'val': 9},
+  {'x': 12, 'val': 9}
+
+
+ship = {'x': 5}
+HEIGHT = 10
+WIDTH = 40
+BULLET_SPEED = 0.6
+
+def move ():
+  pass 
+  
+def draw ():
+  print ('*', shift + 'D')
+
+
+def speet ():
+  pass
+
+
+<<<<<< HEAD
+something
+
+
+while True: 
+  move ()
+  draw ()
+  time.sleep (0.1)  
